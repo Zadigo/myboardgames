519bbcb2a1273063ed722a42c403f985595d3f82
Zadigo <pendenquejohn@gmail.com>
2026-06-22 13:52:30 +0200

    Create base game

--- services/flipseven/main.go
+++ services/flipseven/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"context"
+	"os"
+	"os/signal"
 
+	"github.com/Zadigo/flipseven/internal/app"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	app := app.NewApp(ctx, ".")
+	err := app.Start()
+
+	if err != nil {
+		panic(err)
+	}
 }
