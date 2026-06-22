7a04ebb6e4e9eab5ca3476f9a2a6744f789b191b
Zadigo <pendenquejohn@gmail.com>
2026-06-22 13:50:28 +0200

    Create default base game

---
+++ services/flipseven/internal/app/app.go
@@ -0,0 +1,109 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/Zadigo/flipseven/internal/models"
+	"github.com/go-chi/chi/v5"
+	"github.com/redis/go-redis/v9"
+)
+
+type App struct {
+	ctx         context.Context
+	baseDir     string
+	redisClient *redis.Client
+	router      *chi.Mux
+	errorCh     chan error
+	appConfig   models.AppConfigInterface
+}
+
+func (app *App) Start() error {
+	log.Print("🚀 Starting Goemailer service...")
+
+	if app.redisClient == nil {
+		return fmt.Errorf("Redis client is not initialized")
+	} else {
+		log.Println("✅ Connected to Redis successfully")
+	}
+
+	if app.ctx == nil {
+		return fmt.Errorf("Context is not initialized")
+	}
+
+	port, err := strconv.ParseUint(os.Getenv("GO_PORT"), 10, 16)
+	if err != nil {
+		return fmt.Errorf("🔴 Invalid port: %w", err)
+	}
+
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: app.router,
+	}
+
+	go func() {
+		log.Printf("✅ Goemailer service is running on port %d", port)
+		app.errorCh <- server.ListenAndServe()
+	}()
+
+	select {
+	case err := <-app.errorCh:
+		return fmt.Errorf("HTTP server error: %v", err)
+	case <-app.ctx.Done():
+		timeoutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		app.redisClient.Close()
+
+		log.Println("🔴 Shutting down Goemailer service...")
+		return server.Shutdown(timeoutCtx)
+	}
+}
+
+func (app *App) GetBaseDir() string {
+	return app.baseDir
+}
+
+func (app *App) GetContext() context.Context {
+	return app.ctx
+}
+
+func (app *App) GetConfig() models.AppConfigInterface {
+	return app.appConfig
+}
+
+func (app *App) GetRedisClient() *redis.Client {
+	return app.redisClient
+}
+
+// NewApp initializes and returns a new instance of the App struct
+// with the provided context and base directory. It also sets up the Redis client
+// and service registry.
+func NewApp(ctx context.Context, baseDir string) models.AppInterface {
+	app := &App{
+		ctx:       ctx,
+		baseDir:   baseDir,
+		router:    nil,
+		errorCh:   make(chan error),
+		appConfig: &AppConfig{},
+	}
+
+	app.redisClient = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	cmd := app.redisClient.Ping(ctx)
+	if cmd.Err() != nil {
+		panic(cmd.Err())
+	}
+
+	app.loadRouter()
+	return app
+}
---
+++ services/flipseven/internal/app/router.go
@@ -0,0 +1,29 @@
+package app
+
+import (
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *App) loadRouter() {
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(Cors)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(JsonHeartbeat("/health"))
+	router.Use(middleware.Timeout(60 * time.Second))
+	router.Use(Authorization)
+
+	router.Route("/v1/", app.loadServerRoutes)
+
+	app.router = router
+}
+
+func (app *App) loadServerRoutes(r chi.Router) {
+
+}
